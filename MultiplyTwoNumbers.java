19beea200456ae31422053aeb90dc7740f4b89d5
Balavatti996 <balapranay996@gmail.com>
2023-01-08 16:06:44 +0000

    commit on test branch

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,10 @@
+public class MultiplyTwoNumbers {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 3;
+    int product = num1 * num2;
+
+    System.out.println("The product of num1 and num2 is: " + product);
+  }
+}
+
